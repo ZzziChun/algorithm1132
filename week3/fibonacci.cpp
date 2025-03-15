e1dd8c8b4913bdce8b6177bd12c7eeb3eee8461a
ZzziChun <87875b8787487@gmail.com>
2025-03-15 14:38:54 +0800

    test picture

---
+++ week3/fibonacci.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+const int mod = 1000000;
+int counter = 0;
+
+// using recursive
+long long fibonacci(int n)
+{
+    counter++;
+    if (n <= 1) {
+        return n;
+    } else
+        return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main()
+{
+    int n;
+    printf("using recursive\n");
+    cin >> n;
+    // printf("%06lld\n", fibonacci(n) % mod); // for etutor answer
+    printf("last 6 digits : %lld\n", fibonacci(n) % mod);
+    printf("run times : %d\n", counter);
+
+    return 0;
+}
